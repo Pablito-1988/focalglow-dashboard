df2d784f71624f0550b279a1c5e75a449feaae38
UnDiaDeFuria <pabloar24.7@gmail.com>
2021-09-02 18:37:16 -0300

    Navbar with links to tables

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,29 @@
 import './style-header.css'
 import logo from './assets/img/logo_sinfondo.png'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Header(params) {
     return (
         <>
             <div className='headerWrapper'>
                 <div className='logoFocalGlow' >
-                  <Link to ='/'><img src={logo} alt="Logo Focal Glow" /></Link>  
+                    <Link to='/'><img src={logo} alt="Logo Focal Glow" /></Link>
                 </div>
 
                 <nav className='pageLinks'>
                     <ul>
+                        <li><Link to='/'>Home</Link></li>
+                        <div class="dropdown">
+                            <li class="dropbtn">Tablas</li>
+                            <div class="dropdown-content">
+                                <Link to='/us'>Ventas</Link>
+                                <Link to='/us'>Creación de Prod.</Link>
+                                <Link to='/us'>Edición de Prod.</Link>
+                            </div>
+                        </div>
                         <li><Link to='/us'>Nosotros</Link></li>
-                        <li><Link to='/'>Datos</Link></li>   
+                        
+                        
                     </ul>
                 </nav>
 

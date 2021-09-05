f359974ab484c8456249e52a8a7f6da2e40d583a
UnDiaDeFuria <pabloar24.7@gmail.com>
2021-09-04 22:42:22 -0300

    starting css of modal

--- src/components/PrincipialInfo/Modal/Modal.js
+++ src/components/PrincipialInfo/Modal/Modal.js
@@ -1,24 +1,34 @@
-/* import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom' */
+/* import { useEffect, useState } from 'react' */
+/* import { Link } from 'react-router-dom' */
 import './style-modal.css'
 
-function Modal({onClickClose}) {
- 
+function Modal({ onClickClose }) {
+
+    
+
     return (
         <>
-            
+
             <div id="openModal" class="modalDialog">
                 <div>
                     <button onClick={onClickClose} title="Close" class="close">X</button>
                     <h2 >Detalles del Producto</h2>
-                    <p>hola</p>
-                    <p>Puedes hacer un montón de cosas aquí, como alertas o incluso crear un formulario de registro aquí mismo.</p>
-                    
-                    
+                    <div className='contentWrapper'>
+                        <div className='imageContainer'>
+                            <img src='#' alt='productImg'/>
+                        </div>
+                        <div className='infoContainer'>
+                            <p>hola</p>
+                            <p>Puedes hacer un montón de cosas aquí, como alertas o incluso crear un formulario de registro aquí mismo.</p>
+                        </div>
+                    </div>
+
+
+
                 </div>
             </div>
         </>
     )
 }
 
- export default Modal
+export default Modal

48b218f6d0ecb2ba4ef3ceafc6f86c151a98cf9b
UnDiaDeFuria <pabloar24.7@gmail.com>
2021-09-07 16:46:38 -0300

    changes on css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,7 +16,6 @@ function Header(params) {
                         <div class="dropdown">
                             <li class="dropbtn">Tablas</li>
                             <div class="dropdown-content">
-                                <Link to='/us'>Productos</Link>
                                 <Link to='/us'>Ventas</Link>
                                 <Link to='/us'>Creación de Prod.</Link>
                                 <Link to='/us'>Edición de Prod.</Link>

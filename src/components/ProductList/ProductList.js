7efc90eba93348f643c720c61568ff30f51111d6
Mars <martinianol@outlook.com>
2021-09-10 17:48:25 -0300

    Paginado 95%

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -10,25 +10,26 @@ function ProductList() {
   const [displayProductModal, setDisplayProductModal] = useState(false)
   const [productDetail, setProductDetail] = useState({})
 
+  const [limit, setLimit] = useState(10)
   const [page, setPage] = useState(1)
   const [prevPage, setPrevPage] = useState(1)
   const [nextPage, setNextPage] = useState(2)
+  const [pageQuantity, setPageQuantity] = useState()
 
   useEffect(() => {
-    getProducts(page)
-  }, [])
+    getProducts(page, limit)
+  }, [limit, page])
 
-  function getProducts(page) {
-
-    const limit = 10
+  function getProducts(page, limit) {
 
     fetch(`/api/products/page/${page}/limit/${limit}`)
       .then(result => result.json())
       .then(response => {
         setProducts(response.data)
-        setPrevPage(page - 1)
-        setNextPage(page + 1)
-        setPage(page + 1)
+        page - 1 === 0 ? setPrevPage(1) : setPrevPage(page - 1)
+        page + 1 > response.meta.pageQuantity ? setNextPage(response.meta.pageQuantity) : setNextPage(page + 1)
+        setPage(page)
+        setPageQuantity(response.meta.pageQuantity)
       })
       .catch((err) => {
         console.log(err);
@@ -43,7 +44,7 @@ function ProductList() {
       fetch(`/api/products/${id}`, { method: 'DELETE' })
         .then(result => result.json())
         .then(response => {
-          getProducts()
+          getProducts(page, limit)
         })
     }
   }
@@ -59,14 +60,14 @@ function ProductList() {
   }
 
   function showProductModal() {
-
     setDisplayProductModal(true)
   }
+
   function hideProductModal() {
     setDisplayProductModal(false)
   }
 
-
+  const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
 
   const load = (
     <tr key="-1">
@@ -81,9 +82,6 @@ function ProductList() {
       <td><i class="far fa-trash-alt"></i></td>
     </tr>
   )
-
-  const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
-  console.log(productDetail)
 
   return (
 
@@ -108,54 +106,62 @@ function ProductList() {
           <th>Editar</th>
           <th>Borrar</th>
         </tr>
+        <tbody>
+          {loading ? load : products.map((element, index) => {
 
-        {loading ? load : products.map((element, index) => {
+            return (
+              <tr key={index}>
+                <td className="center">{element.id}</td>
+                <td className="center">{element.category.name}</td>
+                <td>{element.name}</td>
+                <td>${toThousand(element.price)}</td>
+                <td className="center">{element.quantity}</td>
+                <td className="center"><img alt="product" width="80px" src={`/img/${element.images[0].name}`} /></td>
+                <td className="center">
+                  <button onClick={() => showProductDetail(element.id)} className="search"><i className="fas fa-search"></i></button>
+                </td>
+                <td className="center">
+                  <Link to={{ pathname: `http://localhost:3000/product/${element.id}/edit` }} target="_blank" >
+                    <i className="far fa-edit"></i>
+                  </Link>
+                </td>
 
-          return (
-            <tr key={index}>
-              <td className="center">{element.id}</td>
-              <td className="center">{element.category.name}</td>
-              <td>{element.name}</td>
-              <td>${toThousand(element.price)}</td>
-              <td className="center">{element.quantity}</td>
-              <td className="center"><img alt="product" width="80px" src={`/img/${element.images[0].name}`} /></td>
-              <td className="center">
-                <button onClick={() => showProductDetail(element.id)} className="search"><i className="fas fa-search"></i></button>
-              </td>
-              <td className="center">
-                <Link to={{ pathname: `http://localhost:3000/product/${element.id}/edit` }} target="_blank" >
-                  <i className="far fa-edit"></i>
-                </Link>
-              </td>
-
-              <td className="center">
-                <button onClick={() => deleteProduct(element.id)} variant="danger" className="trash" type="submit"><i class="far fa-trash-alt"></i></button>
-              </td>
+                <td className="center">
+                  <button onClick={() => deleteProduct(element.id)} variant="danger" className="trash" type="submit"><i class="far fa-trash-alt"></i></button>
+                </td>
 
 
-            </tr>
+              </tr>
 
-          )
-        })
-        }
+            )
+          })
+          }
+        </tbody>
+
 
       </table>
+      <caption>Productos por vista:  <select value={limit} onChange={event => {
+        setLimit(event.target.value)
+      }}>
+        <option value="5"> 5</option>
+        <option value="10"> 10</option>
+        <option value="15"> 15</option>
+        <option value="20"> 20</option>
+      </select></caption>
 
       <div className="page">
-        <button onClick={() => getProducts(prevPage)} className="offset" value="1">
+        <button onClick={() => getProducts(prevPage, limit)} className="offset">
           <i className="fas fa-arrow-left"></i>
         </button>
-        <button type="submit" className="offset white" value="1">
+        <button type="submit" className="offset white">
           {page}
         </button>
-        <button onClick={() => getProducts(nextPage)} className="offset" value="2">
+        <button onClick={() => getProducts(nextPage, limit)} className="offset">
           <i className="fas fa-arrow-right"></i>
         </button>
       </div>
 
-
-
-
+      <caption>Cantidad de páginas: {pageQuantity}</caption>
 
     </section>
   )

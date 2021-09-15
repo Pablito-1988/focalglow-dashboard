dc8187bf9d8d92ff53fd1d6abd8c96809101be6e
gonzaggb <gonzaggb@hotmail.com>
2021-09-15 10:57:01 -0300

    info apaisada y modificacion css

--- src/components/Sales/InfoContainer.js
+++ src/components/Sales/InfoContainer.js
@@ -1,15 +1,12 @@
-import ProductModal from '../ProductModal/ProductModal'
 import { useState, useEffect } from 'react'
 import Table from '../Table/Table'
 import './style-infoContainer.css'
 
 export default function InfoContainer() {
-    const [productModal, setProductModal] = useState(false)
     const [mostSold, setMostSold] = useState([])
     const [mostColumns, setMostColumns] = useState([])
     const [lastColumns, setLastColumns] = useState([])
     const [lastSold, setLastSold] = useState([])
-    const [modalInfo, setModalInfo] = useState([])
 
     useEffect(() => {
         fetch('http://localhost:3000/api/checkout/listBySold/1/limit/5')
@@ -37,15 +34,21 @@ export default function InfoContainer() {
 
     return (
         <div class='product-info-button'>
-            <h2>Productos más Vendidos</h2>
-            {mostSold.length > 0 ?
-                <Table columnName={mostColumns} tableInfo={mostSold} /> :
-                <span>Aún no hay productos vendidos</span>}
-            <h2>Últimos productos vendidos</h2>
+            <article>
+                <h2>Productos más Vendidos</h2>
+                {mostSold.length > 0 ?
+                    <Table columnName={mostColumns} tableInfo={mostSold} /> :
+                    <span>Aún no hay productos vendidos</span>}
+            </article>
+            <article>
 
-            {lastSold.length > 0 ?
-                <Table columnName={lastColumns} tableInfo={lastSold} /> :
-                <span>Aún no hay productos vendidos</span>}
+                <h2>Últimos productos vendidos</h2>
+
+                {lastSold.length > 0 ?
+                    <Table columnName={lastColumns} tableInfo={lastSold} /> :
+                    <span>Aún no hay productos vendidos</span>}
+            </article>
+
         </div>)
 
 }

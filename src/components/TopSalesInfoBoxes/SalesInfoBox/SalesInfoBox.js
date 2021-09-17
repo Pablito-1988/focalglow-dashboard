a1ca3d160d7f41741f0188245b1c4ef77cf20282
gonzaggb <gonzaggb@hotmail.com>
2021-09-17 20:30:45 -0300

    separador de mil

--- src/components/TopSalesInfoBoxes/SalesInfoBox/SalesInfoBox.js
+++ src/components/TopSalesInfoBoxes/SalesInfoBox/SalesInfoBox.js
@@ -1,12 +1,12 @@
 function SalesInfoBox(props) {
- 
+    const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
     return(
         <div className='infoBox' >
             <h4>
                 Total de {props.name}:
             </h4>
             <h4 className='qtyProducts'>
-                   {props.info}
+                   {toThousand(props.info)}
             </h4>
         </div>
     )
